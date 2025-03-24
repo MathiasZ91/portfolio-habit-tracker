b640cc11704eeff6f5f1a0edbcab64df2c703c89
MathiasZ91git config --global user.email mzeibig@hotmail.deit commit -minitialgit commit -minitialgit commit -minitialgit config --global user.name MathiasZ91 <mzeibig@hotmail.de>
2025-03-24 12:45:54 +0100

    React Router v7

---
+++ src/pages/DailyGoalsPage.jsx
@@ -0,0 +1,10 @@
+const DailyGoalsPage = () => {
+  return (
+    <div>
+      <h1>Welcome to Your Daily Goals!</h1>
+      {/* Add more content related to Daily Goals here */}
+    </div>
+  );
+}
+
+export default DailyGoalsPage;
